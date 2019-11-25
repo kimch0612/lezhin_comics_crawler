bd7644f37c14f144b07a36704d469119f95f7c98
Changhwan Kim <kimch061279@gmail.com>
2019-11-25 18:49:42 +0900

    버그 수정 및 안정화

--- crawling.py
+++ crawling.py
@@ -3,21 +3,16 @@ import json
 import urllib.request
 import os
 import shutil
-from img2pdf import convert
+from PIL import Image
+import glob
 
 print('Welcome to Lezhin Comics Crawler')
 print("※ 2017년 3월 10일 이후에 나온 만화는 다운로드가 불가합니다")
 
 
-def download(url, file_name):
-    with open(file_name, "wb") as file:
-        response = get(url)
-        file.write(response.content)
-
-
+token = input('레진 계정의 토큰 값을 입력하세요 : ')
 name = input('만화의 영문 이름을 입력하세요 : ')
 episode = input('다운로드 받을 에피소드의 회차 범위를 지정해주세요\n(입력 예 : 1~3) : ')
-token = input('레진 계정의 토큰값을 입력하세요 : ')
 csb = episode
 sgw = csb.split("~")
 
@@ -30,28 +25,57 @@ try:
 except:
     pass
 
-for a in range(int(sgw[0]), int(sgw[1])+1):
+print("만화 정보를 다운로드 중입니다...")
+for y in range(int(sgw[0]), int(sgw[1])+1):
+    def download(url, file_name):
+        with open(file_name, "wb") as file:
+            response = get(url)
+            file.write(response.content)
     if __name__ == '__main__':
-        url = "http://cdn.lezhin.com/episodes/%s/%s.json?access_token=%s" % (name, a, token)
-        download(url, "%s\\temp\\%s.json" % (name, a))
+        url = "http://cdn.lezhin.com/episodes/%s/%s.json?access_token=%s" % (name, y, token)
+        download(url, "%s\\temp\\%s.json" % (name, y))
 
-    with open('%s\\temp\\%s.json'%(name, a), 'rt', encoding='UTF8') as json_file:
+for a in range(int(sgw[0]), int(sgw[1])+1):
+    os.chdir(name)
+    with open('temp\\%s.json'%(a), 'rt', encoding='UTF8') as json_file:
         json_data = json.load(json_file)
         json_number = json_data["cut"]
         cut = json_number
 
-    print('%s화 다운로드를 시작합니다.\n%s화의 총 컷 수는 %s장입니다.'%(a, a, cut))
+    print('-----%s화 다운로드를 시작합니다.-----\n%s화의 총 이미지 수는 %s장입니다.'%(a, a, cut))
 
     try:
-        os.mkdir("%s\\%s화"%(name, a))
+        os.mkdir("%s화"%(a))
     except:
         pass
 
+    home = str(a) + "화"
     for i in range(1, cut+1):
-        print('%s번째 컷 중'%(cut) + " %s"%(i) + ' 번째 컷 다운로드 중...')
-        urllib.request.urlretrieve("http://cdn.lezhin.com/episodes/%s/%s/contents/%s?access_token=%s"%(name, a, i, token), "%s\\%s화\\%s.png"%(name, a, i))
+        print('%s번째 이미지 중'%(cut) + " %s"%(i) + ' 번째 이미지 다운로드 중...')
+        urllib.request.urlretrieve("http://cdn.lezhin.com/episodes/%s/%s/contents/%s?access_token=%s"%(name, a, i, token), "%s화\\%s.png"%(a, i))
     print('%s화 다운로드 완료.'%(a))
 
-print('임시폴더 삭제 중...')
-shutil.rmtree(r"%s\\temp"%(name))
+    dir = "%s화"%(a)
+    print("pdf 변환 중...")
+    prefix = ""
+    min_range = 1
+    max_range = cut
+    os.chdir(dir)
+    suffix = ".png"
+    out_fname = "%s화.pdf"%(a)
+
+    images = []
+    for z in range(min_range, max_range + 1):
+        fname = prefix + str(z) + suffix
+        im = Image.open(fname)
+        if im.mode == "RGBA":
+            im = im.convert("RGB")
+        images.append(im)
+    os.chdir('..')
+    images[0].save(out_fname, save_all=True, quality=100, append_images=images[1:])
+    os.chdir('..')
+
+print('임시파일 삭제 중...')
+os.chdir(name)
+shutil.rmtree(r"temp")
 print('완료!!')
