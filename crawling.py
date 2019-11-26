5e17d7536e66c968ab245c887706f289dd94a717
Changhwan Kim <kimch061279@gmail.com>
2019-11-26 14:10:01 +0900

    버그 수정 및 기능 개선

--- crawling.py
+++ crawling.py
@@ -14,6 +14,12 @@ import glob
 print('Welcome to Lezhin Comics Crawler')
 print("※ 2017년 3월 10일 이후에 나온 만화는 다운로드가 불가합니다")
 
+"""
+토큰은 숫자와 소문자 영어로 구성되어 있으며 xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx 형식으로 이루어졌습니다
+이름의 경우에는 다운 받으려는 만화로 들어가면 URL에 나와있습니다
+레진의 계정 토큰 값은 아무 만화나 들어가고 크롬 기준으로 F12 버튼을 눌러 html 분석기를 켠 뒤에
+token 이라고 검색하면 나오는 값을 사용하면 됩니다
+"""
 
 token = input('레진 계정의 토큰 값을 입력하세요 : ')
 name = input('만화의 영문 이름을 입력하세요 : ')
