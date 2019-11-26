510d1133b14df513d28a6d65f986ba10cce19c12
Changhwan Kim <kimch061279@gmail.com>
2019-11-26 14:18:51 +0900

    버그 수정 및 기능 개선

---
+++ crawler_new-ver.py
@@ -0,0 +1,87 @@
+"""
+programmed by GreenF
+(c) 2019. GreenF. All rights reserved
+"""
+
+from requests import get
+import json
+import urllib.request
+import os
+from PIL import Image
+
+print('Welcome to Lezhin Comics Crawler')
+print("※ 2017년 3월 10일 이후에 연재된 만화를 다운받을 수 있는 크롤러입니다")
+
+"""
+토큰은 숫자와 소문자 영어로 구성되어 있으며 xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx 형식으로 이루어졌습니다
+이름의 경우에는 다운 받으려는 만화로 들어가면 URL에 나와있습니다
+만화의 숫자 코드와 에피소드의 숫자 코드, 레진의 계정 토큰 값은 만화에서 F12를 누르고,
+크롬 기준 sources탭에서 cdn.lezhin.com -> v2 -> comics에 들어가면 알 수 있습니다
+comics 바로 아래에 있는 폴더가 만화의 숫자 코드이며 그 안에 episodes 폴더가 있는데,
+거기로 들어가면 에피소드의 숫자 코드를 알 수 있습니다
+이미지 파일의 링크를 복사해서 보면 위의 정보들을 한 눈에 볼 수 있습니다
+https://cdn.lezhin.com/v2/comics/만화_숫자코드/episodes/에피_숫자코드/contents/scrolls/이미지_번호?access_token=토큰_값
+"""
+
+while True :
+    name = input("만화의 영어 이름을 입력하세요 : ")
+    name_code = input("만화의 숫자 코드를 입력하세요\n(숫자는 총 16자리입니다) : ")
+    episode = input("에피소드 회차를 입력하세요\n(ex. 24화라면 24만 입력) : ")
+    episode_code = input("에피소드의 숫자 코드를 입력하세요\n(숫자는 총 16자리입니다) : ")
+    token = input("레진 계정의 토큰 값을 입력하세요 : ")
+
+    try:
+        os.mkdir("%s"%(name))
+    except:
+        pass
+    try:
+        os.mkdir("%s\\%s화"%(name, episode))
+    except:
+        pass
+
+    print("만화 정보를 다운로드 중입니다...", end="")
+    def download(url, file_name):
+        with open(file_name, "wb") as file:
+            response = get(url)
+            file.write(response.content)
+
+
+    if __name__ == '__main__':
+            url = "http://cdn.lezhin.com/episodes/%s/%s.json?access_token=%s" % (name, episode, token)
+            download(url, "%s\\%s.json" % (name, episode))
+
+    with open('%s\\%s.json'%(name, episode), 'rt', encoding='UTF8') as json_file:
+            json_data = json.load(json_file)
+            json_number = json_data["cut"]
+            cut = json_number
+    print("완료")
+    print('-----%s화 다운로드를 시작합니다.-----\n%s화의 총 이미지 수는 %s장입니다.'%(episode, episode, cut))
+
+    for i in range(1, cut+1):
+        print('%s번째 이미지 중' % (cut) + " %s" % (i) + ' 번째 이미지 다운로드 중...', end='')
+        urllib.request.urlretrieve("https://cdn.lezhin.com/v2/comics/%s/episodes/%s/contents/scrolls/%s?access_token=%s" % (name_code, episode_code, i, token), "%s\\%s화\\%s.png" % (name, episode, i))
+        print("완료")
+    print('%s화 다운로드 완료.' % (episode))
+
+    dir = "%s화" % (episode)
+    print("pdf 변환 중...", end='')
+    prefix = ""
+    min_range = 1
+    max_range = cut
+    os.chdir(name)
+    os.chdir(dir)
+    suffix = ".png"
+    out_fname = "%s화.pdf" % (episode)
+
+    images = []
+    for z in range(min_range, max_range + 1):
+        fname = prefix + str(z) + suffix
+        im = Image.open(fname)
+        if im.mode == "RGBA":
+            im = im.convert("RGB")
+        images.append(im)
+    os.chdir('..')
+    images[0].save(out_fname, save_all=True, quality=100, append_images=images[1:])
+    os.chdir('..')
+    print("완료")
+
---
+++ crawler_old-ver.py
@@ -0,0 +1,95 @@
+"""
+programmed by GreenF
+(c) 2019. GreenF. All rights reserved
+"""
+
+from requests import get
+import json
+import urllib.request
+import os
+import shutil
+from PIL import Image
+import glob
+
+print('Welcome to Lezhin Comics Crawler')
+print("※ 2017년 3월 10일 이후에 나온 만화는 다운로드가 불가합니다")
+
+"""
+토큰은 숫자와 소문자 영어로 구성되어 있으며 xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx 형식으로 이루어졌습니다
+이름의 경우에는 다운 받으려는 만화로 들어가면 URL에 나와있습니다
+레진의 계정 토큰 값은 아무 만화나 들어가고 크롬 기준으로 F12 버튼을 눌러 html 분석기를 켠 뒤에
+token 이라고 검색하면 나오는 값을 사용하면 됩니다
+"""
+
+token = input('레진 계정의 토큰 값을 입력하세요 : ')
+name = input('만화의 영문 이름을 입력하세요 : ')
+episode = input('다운로드 받을 에피소드의 회차 범위를 지정해주세요\n(입력 예 : 1~3) : ')
+csb = episode
+sgw = csb.split("~")
+
+try:
+    os.mkdir("%s"%(name))
+except:
+    pass
+try:
+    os.mkdir("%s\\temp"%(name))
+except:
+    pass
+
+print("만화 정보를 다운로드 중입니다...", end="")
+for y in range(int(sgw[0]), int(sgw[1])+1):
+    def download(url, file_name):
+        with open(file_name, "wb") as file:
+            response = get(url)
+            file.write(response.content)
+    if __name__ == '__main__':
+        url = "http://cdn.lezhin.com/episodes/%s/%s.json?access_token=%s" % (name, y, token)
+        download(url, "%s\\temp\\%s.json" % (name, y))
+print("완료")
+
+for a in range(int(sgw[0]), int(sgw[1])+1):
+    os.chdir(name)
+    with open('temp\\%s.json'%(a), 'rt', encoding='UTF8') as json_file:
+        json_data = json.load(json_file)
+        json_number = json_data["cut"]
+        cut = json_number
+
+    print('-----%s화 다운로드를 시작합니다.-----\n%s화의 총 이미지 수는 %s장입니다.'%(a, a, cut))
+
+    try:
+        os.mkdir("%s화"%(a))
+    except:
+        pass
+
+    home = str(a) + "화"
+    for i in range(1, cut+1):
+        print('%s번째 이미지 중'%(cut) + " %s"%(i) + ' 번째 이미지 다운로드 중...', end='')
+        urllib.request.urlretrieve("http://cdn.lezhin.com/episodes/%s/%s/contents/%s?access_token=%s"%(name, a, i, token), "%s화\\%s.png"%(a, i))
+        print("완료")
+    print('%s화 다운로드 완료.'%(a))
+
+    dir = "%s화"%(a)
+    print("pdf 변환 중...", end='')
+    prefix = ""
+    min_range = 1
+    max_range = cut
+    os.chdir(dir)
+    suffix = ".png"
+    out_fname = "%s화.pdf"%(a)
+
+    images = []
+    for z in range(min_range, max_range + 1):
+        fname = prefix + str(z) + suffix
+        im = Image.open(fname)
+        if im.mode == "RGBA":
+            im = im.convert("RGB")
+        images.append(im)
+    os.chdir('..')
+    images[0].save(out_fname, save_all=True, quality=100, append_images=images[1:])
+    os.chdir('..')
+    print("완료")
+
+print('임시파일 삭제 중...')
+os.chdir(name)
+shutil.rmtree(r"temp")
+print('완료!!')
