63411ef9f43f0c2fc0421a994d5d030ebb20048f
Changhwan Kim <chals@nsu.ac.kr>
2020-03-15 13:14:57 +0900

    임시 파일이 자동으로 삭제되도록 코드 개선

--- crawler_new-ver.py
+++ crawler_new-ver.py
@@ -41,7 +41,6 @@ while True :
             response = get(url)
             file.write(response.content)
 
-
     if __name__ == '__main__':
             url = "http://cdn.lezhin.com/episodes/%s/%s.json?access_token=%s" % (name, episode, token)
             download(url, "%s\\%s.json" % (name, episode))
@@ -50,6 +49,7 @@ while True :
             json_data = json.load(json_file)
             json_number = json_data["cut"]
             cut = json_number
+            os.remove('%s\\%s.json')%(name, episode)
     print("완료")
     print('-----%s화 다운로드를 시작합니다.-----\n%s화의 총 이미지 수는 %s장입니다.'%(episode, episode, cut))
 
@@ -60,7 +60,7 @@ while True :
     print('%s화 다운로드 완료.' % (episode))
 
     dir = "%s화" % (episode)
-    print("pdf 변환 중...", end='')
+    print("pdf 생성 중...", end='')
     prefix = ""
     min_range = 1
     max_range = cut
