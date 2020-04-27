1024294e1e4c7ddad5b13b228461bb216e63f3ce
Changhwan Kim <chals@nsu.ac.kr>
2020-04-27 16:01:46 +0900

    소스코드 최적화

--- crawler_selenium-ver.py
+++ crawler_selenium-ver.py
@@ -13,7 +13,7 @@ from img2pdf import convert
 import sys
 
 print("Welcome To Lezhin Comics Crawler - Selenium version.")
-print("Crawler Ver : Dev 3.2")
+print("Crawler Ver : Dev 3.3")
 
 erran = ("크롤러에 오류가 발생하여 다운로드를 재시작하려 했으나, 해결이 불가한 오류가 발생하여 크롤러를 종료합니다.\n"
          "만약 지속적으로 동일한 오류가 발생한다면 아래의 내용들을 복사하여 에러 코드를 개발자에게 보내주세요.\n"
