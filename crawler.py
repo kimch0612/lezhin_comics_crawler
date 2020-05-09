11951d68b94e047d9cf3a9e45f35ccb58723b43f
Changhwan Kim <chals@nsu.ac.kr>
2020-05-09 22:19:11 +0900

    업데이트 롤백

--- crawler.py
+++ crawler.py
@@ -1,17 +1,2 @@
-import time
-import getpass
-import os
-import json
-import urllib.request
-import shutil
-import natsort
-import urllib.error
-import sys
-import chromedriver_autoinstaller
 import subprocess
-from selenium import webdriver
-from bs4 import BeautifulSoup
-from requests import get
-from tqdm import tqdm
-from img2pdf import convert
 subprocess.call('python crawler_selenium-ver.py', shell=True)
